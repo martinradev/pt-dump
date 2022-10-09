ccbfbc9a8a156c29acbdbbe2824b08a503264694
Martin Radev <martin.b.radev@gmail.com>
2022-10-09 18:16:43 +0300

    Update py-interfaces

--- gdb-pt-dump-rust-py-interface/src/lib.rs
+++ gdb-pt-dump-rust-py-interface/src/lib.rs
@@ -1,17 +1,18 @@
 use std::sync::Arc;
 
+use pt_dump_lib::filter::page_range_filter;
 use pt_dump_lib::filter::page_range_filter::PageRangeFilterX86;
-use pt_dump_lib::pt::*;
+use pt_dump_lib::memory::memory::MemoryView;
+use pt_dump_lib::print::printer::{Printer, X86Writer};
 use pt_dump_lib::pt::arm;
 use pt_dump_lib::pt::arm::ArmPageRange;
 use pt_dump_lib::pt::page_range::*;
 use pt_dump_lib::pt::x86::X86PageRange;
-use pt_dump_lib::print::printer::{Printer, X86Writer};
-use pt_dump_lib::search::bytes_search;
-use pt_dump_lib::filter::page_range_filter;
-use pyo3::prelude::*;
-use pyo3::{types::{PyTuple, PyList, PyBytes}};
+use pt_dump_lib::pt::*;
+use pt_dump_lib::search::bytes_search::{self, SearchResult};
 use pyo3::exceptions::PyTypeError;
+use pyo3::prelude::*;
+use pyo3::types::{PyBytes, PyList, PyTuple};
 use qemu_memory::QemuMemoryView;
 mod qemu_memory;
 
@@ -22,7 +23,7 @@ struct PyFilter_X86 {
     user_accessible: Option<bool>,
     va_range: Option<(Option<u64>, Option<u64>)>,
     has_address: Option<u64>,
-    only_superuser_accessible: Option<bool>
+    only_superuser_accessible: Option<bool>,
 }
 
 trait PageTable<PageType> {
@@ -98,9 +99,9 @@ fn collect_ram_ranges(phys_ranges: &PyList) -> Result<Vec<qemu_memory::RamRange>
         if tuple.len() != 3 {
             return Err(PyTypeError::new_err("Expected a tuple"));
         }
-        let gpa_start : usize = tuple.get_item(0).unwrap().extract().unwrap();
-        let gpa_extent : usize = tuple.get_item(1).unwrap().extract().unwrap();
-        let hva : usize = tuple.get_item(2).unwrap().extract().unwrap();
+        let gpa_start: usize = tuple.get_item(0).unwrap().extract().unwrap();
+        let gpa_extent: usize = tuple.get_item(1).unwrap().extract().unwrap();
+        let hva: usize = tuple.get_item(2).unwrap().extract().unwrap();
         phys_ranges_vec.push(qemu_memory::RamRange::new(gpa_start, gpa_extent, hva));
     }
     Ok(phys_ranges_vec)
@@ -113,18 +114,30 @@ fn create_memory_view(fd: i32, phys_ranges: &PyList) -> Result<QemuMemoryView, P
         Err(err) => return Err(err),
     };
     let memory_view = match qemu_memory::QemuMemoryView::new(fd, &ram_ranges, true) {
-        Err(err) => return Err(pyo3::exceptions::PyIOError::new_err("Couldn't open QEMU mem fd")),
-        Ok(res) => res
+        Err(err) => {
+            return Err(pyo3::exceptions::PyIOError::new_err(
+                "Couldn't open QEMU mem fd",
+            ))
+        }
+        Ok(res) => res,
     };
     Ok(memory_view)
 }
 
-fn pages_to_ranges<PageType: GenericPage + Clone, RangeType: GenericPageRange + From<PageType>>(pages: &Vec<PageType>) -> Vec<RangeType> {
+fn pages_to_ranges<PageType: GenericPage + Clone, RangeType: GenericPageRange + From<PageType>>(
+    pages: &Vec<PageType>,
+) -> Vec<RangeType> {
     pages.iter().map(|x| RangeType::from(x.clone())).collect()
 }
 
 #[pyfunction]
-fn parse_page_table_x86_32(fd: i32, cr3: u64, pae: bool, pse: bool, phys_ranges: &PyList) -> PyResult<PageTableX86> {
+fn parse_page_table_x86_32(
+    fd: i32,
+    cr3: u64,
+    pae: bool,
+    pse: bool,
+    phys_ranges: &PyList,
+) -> PyResult<PageTableX86> {
     let mut memory_view = create_memory_view(fd, &phys_ranges)?;
     let pages = x86::collect_pages(x86::X86Flavour::X86, &mut memory_view, cr3, pse, pae);
     if let Ok(pages_ok) = pages {
@@ -135,7 +148,13 @@ fn parse_page_table_x86_32(fd: i32, cr3: u64, pae: bool, pse: bool, phys_ranges:
 }
 
 #[pyfunction]
-fn parse_page_table_x86_64(fd: i32, cr3: u64, pae: bool, pse: bool, phys_ranges: &PyList) -> PyResult<PageTableX86> {
+fn parse_page_table_x86_64(
+    fd: i32,
+    cr3: u64,
+    pae: bool,
+    pse: bool,
+    phys_ranges: &PyList,
+) -> PyResult<PageTableX86> {
     let mut memory_view = create_memory_view(fd, &phys_ranges)?;
     let pages = x86::collect_pages(x86::X86Flavour::X64, &mut memory_view, cr3, pse, pae);
     if let Ok(pages_ok) = pages {
@@ -146,12 +165,19 @@ fn parse_page_table_x86_64(fd: i32, cr3: u64, pae: bool, pse: bool, phys_ranges:
 }
 
 #[pyfunction]
-fn parse_page_table_aarch64(fd: i32, pt_pa: u64, phys_ranges: &PyList) -> PyResult<PageTableAarch64> {
+fn parse_page_table_aarch64(
+    fd: i32,
+    pt_pa: u64,
+    phys_ranges: &PyList,
+) -> PyResult<PageTableAarch64> {
     let mut memory_view = create_memory_view(fd, &phys_ranges)?;
     let arm_context = arm::ArmContext::new(arm::ArmFlavour::Arm64, arm::Granularity::Pt4k, 0, 0);
     let pages = arm::collect_pages(&arm_context, &mut memory_view, pt_pa);
     if let Ok(pages_ok) = pages {
-        Ok(PageTableAarch64::new(pages_to_ranges(&pages_ok), memory_view))
+        Ok(PageTableAarch64::new(
+            pages_to_ranges(&pages_ok),
+            memory_view,
+        ))
     } else {
         return Err(PyTypeError::new_err("Failed to collect pages"));
     }
@@ -159,7 +185,7 @@ fn parse_page_table_aarch64(fd: i32, pt_pa: u64, phys_ranges: &PyList) -> PyResu
 
 #[pyfunction]
 fn filter_page_table_x86(table: &mut PageTableX86, filter: &PyAny) -> PyResult<PageTableX86> {
-    let filter : PyFilter_X86 = filter.extract()?;
+    let filter: PyFilter_X86 = filter.extract()?;
     let mut pt_filter = PageRangeFilterX86::new();
     if let Some(e) = filter.executable {
         pt_filter.set_executable(e);
@@ -181,14 +207,30 @@ fn filter_page_table_x86(table: &mut PageTableX86, filter: &PyAny) -> PyResult<P
     }
 
     let memory_view = table.get_memory_view().clone();
-    Ok(PageTableX86::new(page_range_filter::filter_x86_ranges(table.get_ranges(), &pt_filter), memory_view))
+    Ok(PageTableX86::new(
+        page_range_filter::filter_x86_ranges(table.get_ranges(), &pt_filter),
+        memory_view,
+    ))
 }
 
-fn search_memory_generic<PageRangeType: GenericPageRange + ToString, PageTableType: PageTable<PageRangeType>>(table: &mut PageTableType, data_py: &PyBytes, max_found: usize) -> PyResult<String> {
-    let data : &[u8] = data_py.as_bytes();
+fn search_memory_generic<
+    PageRangeType: GenericPageRange + ToString,
+    PageTableType: PageTable<PageRangeType>,
+>(
+    table: &mut PageTableType,
+    data: &[u8],
+    alignment: u64,
+    max_found: usize,
+) -> PyResult<String> {
     let mut memory_view = table.get_memory_view().clone();
     let ranges = table.get_ranges();
-    let search_result = bytes_search::search_memory_generic(&data, ranges, &mut memory_view, max_found);
+    let search_result = bytes_search::search_memory_generic(
+        &data,
+        ranges,
+        &mut memory_view,
+        Some(alignment),
+        max_found,
+    );
     let occs = search_result.get_results();
     if occs.is_empty() {
         return Ok(String::from("Not found"));
@@ -200,19 +242,100 @@ fn search_memory_generic<PageRangeType: GenericPageRange + ToString, PageTableTy
         if occ.range_index != saved_range.0 {
             saved_range = (occ.range_index, ranges[occ.range_index].to_string())
         }
-        result_str.push_str(&format!("Found at 0x{:016x} in {}\n", occ.va, saved_range.1));
+        result_str.push_str(&format!(
+            "Found at 0x{:016x} in {}\n",
+            occ.va, saved_range.1
+        ));
     }
     Ok(result_str)
 }
 
 #[pyfunction]
-fn search_memory_x86(table: &mut PageTableX86, data_py: &PyBytes, max_found: usize) -> PyResult<String> {
-    search_memory_generic(table, data_py, max_found)
+fn search_memory_x86(
+    table: &mut PageTableX86,
+    data_py: &PyBytes,
+    alignment: u64,
+    max_found: usize,
+) -> PyResult<String> {
+    search_memory_generic(table, data_py.as_bytes(), alignment, max_found)
 }
 
 #[pyfunction]
-fn search_memory_aarch64(table: &mut PageTableAarch64, data_py: &PyBytes, max_found: usize) -> PyResult<String> {
-    search_memory_generic(table, data_py, max_found)
+fn search_memory_aarch64(
+    table: &mut PageTableAarch64,
+    data_py: &PyBytes,
+    alignment: u64,
+    max_found: usize,
+) -> PyResult<String> {
+    search_memory_generic(table, data_py.as_bytes(), alignment, max_found)
+}
+
+#[pyclass]
+struct KaslrInfo {
+    #[pyo3(get)]
+    image_virt: Option<u64>,
+
+    #[pyo3(get)]
+    image_phys: Option<u64>,
+
+    #[pyo3(get)]
+    physmap_virt: Option<u64>,
+}
+
+#[pyfunction]
+fn find_kaslr_linux_x86(table: &mut PageTableX86) -> PyResult<KaslrInfo> {
+    let mut kaslr_info = KaslrInfo {
+        image_virt: None,
+        image_phys: None,
+        physmap_virt: None,
+    };
+
+    let mut filter_and_search = |is_executable: bool| -> Option<(u64, Option<u64>)> {
+        let memory_view = table.get_memory_view().clone();
+        let mut pt_filter = PageRangeFilterX86::new();
+        pt_filter.set_executable(is_executable);
+        pt_filter.set_superuser_accessible(true);
+        let filtered_pt = PageTableX86::new(
+            page_range_filter::filter_x86_ranges(table.get_ranges(), &pt_filter),
+            memory_view,
+        );
+
+        // Next find 2-MiB-aligned pages which has the byte 0x48 at the beginning.
+        let alignment = 2 * 1024 * 1024;
+        let linux_base_needle = [0x48_u8];
+
+        let mut memory_view = table.get_memory_view().clone();
+        let ranges = filtered_pt.get_ranges();
+        let search_result = bytes_search::search_memory_generic(
+            &linux_base_needle,
+            ranges,
+            &mut memory_view,
+            Some(alignment),
+            1,
+        );
+        let occ = search_result.get_results();
+        if occ.is_empty() {
+            None
+        } else {
+            Some((
+                occ[0].va,
+                filtered_pt.get_ranges()[occ[0].range_index].gva_to_gpa(occ[0].va),
+            ))
+        }
+    };
+
+    let image_info = filter_and_search(true);
+    if let Some(image_info_ok) = image_info {
+        kaslr_info.image_virt = Some(image_info_ok.0);
+        kaslr_info.image_phys = image_info_ok.1;
+    }
+
+    let physbase_info = filter_and_search(false);
+    if let Some(physbase_info_ok) = physbase_info {
+        kaslr_info.physmap_virt = Some(physbase_info_ok.0);
+    }
+
+    Ok(kaslr_info)
 }
 
 #[pymodule]
@@ -220,10 +343,20 @@ fn gdb_pt_dump_rust_py_interface(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(parse_page_table_x86_32, m)?)?;
     m.add_function(wrap_pyfunction!(parse_page_table_x86_64, m)?)?;
     m.add_function(wrap_pyfunction!(parse_page_table_aarch64, m)?)?;
-
+    // TODO: riscv64
     m.add_function(wrap_pyfunction!(get_page_table_as_string, m)?)?;
+
     m.add_function(wrap_pyfunction!(filter_page_table_x86, m)?)?;
+    // TODO: aarch64
+    // TODO: riscv64
+
     m.add_function(wrap_pyfunction!(search_memory_x86, m)?)?;
     m.add_function(wrap_pyfunction!(search_memory_aarch64, m)?)?;
+    // TODO: riscv64
+
+    m.add_function(wrap_pyfunction!(find_kaslr_linux_x86, m)?)?;
+    // TODO: aarch64
+    // TODO: riscv64
+
     Ok(())
 }
--- gdb-pt-dump-rust-py-interface/src/qemu_memory.rs
+++ gdb-pt-dump-rust-py-interface/src/qemu_memory.rs
@@ -1,6 +1,6 @@
-use pt_dump_lib::{memory::memory::MemoryView, memory::memory_fd::*, pt::common::Error};
-use nc;
 use lru::LruCache;
+use nc;
+use pt_dump_lib::{memory::memory::MemoryView, memory::memory_fd::*, pt::common::Error};
 use std::num::NonZeroUsize;
 
 #[derive(Copy, Clone, Debug)]
@@ -12,7 +12,11 @@ pub struct RamRange {
 
 impl RamRange {
     pub fn new(gpa_start: usize, gpa_extent: usize, hva: usize) -> Self {
-        Self { gpa_start: gpa_start, gpa_extent: gpa_extent, hva: hva }
+        Self {
+            gpa_start: gpa_start,
+            gpa_extent: gpa_extent,
+            hva: hva,
+        }
     }
 }
 
@@ -24,7 +28,11 @@ pub struct QemuMemoryView {
 }
 
 impl QemuMemoryView {
-    pub fn new(mem_fd: i32, sorted_ram_ranges: &Vec<RamRange>, use_cache: bool) -> Result<Self, Error> { 
+    pub fn new(
+        mem_fd: i32,
+        sorted_ram_ranges: &Vec<RamRange>,
+        use_cache: bool,
+    ) -> Result<Self, Error> {
         let new_owned_fd = match unsafe { nc::dup(mem_fd) } {
             Ok(new_fd) => new_fd,
             Err(err) => return Err(Error::ResourceError),
@@ -37,7 +45,11 @@ impl QemuMemoryView {
         })
     }
 
-    pub fn find_phys_ranges(&self, gpa_start: usize, gpa_end_excl: usize) -> Option<(usize, usize)> {
+    pub fn find_phys_ranges(
+        &self,
+        gpa_start: usize,
+        gpa_end_excl: usize,
+    ) -> Option<(usize, usize)> {
         if gpa_start >= gpa_end_excl {
             return None;
         }
@@ -45,7 +57,11 @@ impl QemuMemoryView {
         let mut end_result = None;
         for i in 0..self.sorted_ram_ranges.len() {
             let ram_range = &self.sorted_ram_ranges[i];
-            if start_result.is_none() && ((ram_range.gpa_start <= gpa_start && ram_range.gpa_start + ram_range.gpa_extent > gpa_start) || (ram_range.gpa_start >= gpa_start))  {
+            if start_result.is_none()
+                && ((ram_range.gpa_start <= gpa_start
+                    && ram_range.gpa_start + ram_range.gpa_extent > gpa_start)
+                    || (ram_range.gpa_start >= gpa_start))
+            {
                 start_result = Some(i);
             }
             if ram_range.gpa_start < gpa_end_excl {
@@ -107,7 +123,12 @@ impl MemoryView for QemuMemoryView {
         }
     }
 
-    fn read_block_inplace(&mut self, gpa: usize, block_size: usize, block: &mut [u8]) -> Result<(), Error> {
+    fn read_block_inplace(
+        &mut self,
+        gpa: usize,
+        block_size: usize,
+        block: &mut [u8],
+    ) -> Result<(), Error> {
         if self.use_cache {
             let value = self.cache.get(&(gpa, block_size));
             if let Some(value_ok) = value {
@@ -122,12 +143,20 @@ impl MemoryView for QemuMemoryView {
                 let range = &self.sorted_ram_ranges[index];
                 let gpa_start = std::cmp::max(gpa, range.gpa_start);
                 let gpa_offset = gpa_start - range.gpa_start;
-                let max_to_read = std::cmp::min(range.gpa_extent - gpa_offset, block_size - (gpa_start - gpa));
+                let max_to_read = std::cmp::min(
+                    range.gpa_extent - gpa_offset,
+                    block_size - (gpa_start - gpa),
+                );
                 let va_start = range.hva + gpa_offset;
-                let cur_pos = gpa_start-gpa;
+                let cur_pos = gpa_start - gpa;
 
                 // Ignore error.
-                let result = read_inline_block_from_fd(self.mem_fd, va_start, max_to_read, &mut block[cur_pos..]);
+                let result = read_inline_block_from_fd(
+                    self.mem_fd,
+                    va_start,
+                    max_to_read,
+                    &mut block[cur_pos..],
+                );
             }
             self.cache.put((gpa, block_size), Vec::from(block));
             Ok(())
@@ -173,7 +202,10 @@ fn test_qemu_memory_view() {
     assert_eq!(Some((0, 1)), range);
 
     let range = qemuMem.find_phys_ranges(0x11a000, 0x20000000);
-    assert_eq!(Some((sorted_ranges.len() - 1, sorted_ranges.len() - 1)), range);
+    assert_eq!(
+        Some((sorted_ranges.len() - 1, sorted_ranges.len() - 1)),
+        range
+    );
 }
 
 #[test]
@@ -181,7 +213,17 @@ fn test_memory_read() {
     let pid = unsafe { nc::getpid() };
     assert_eq!(true, pid > 0);
     let sz = 0x10000;
-    let base_va = unsafe { nc::mmap(0, sz, nc::PROT_READ | nc::PROT_WRITE, nc::MAP_PRIVATE | nc::MAP_ANONYMOUS, -1, 0) }.unwrap();
+    let base_va = unsafe {
+        nc::mmap(
+            0,
+            sz,
+            nc::PROT_READ | nc::PROT_WRITE,
+            nc::MAP_PRIVATE | nc::MAP_ANONYMOUS,
+            -1,
+            0,
+        )
+    }
+    .unwrap();
     let base_va_u8 = unsafe { std::slice::from_raw_parts_mut(base_va as *mut u8, sz) };
     for page_index in 0..(sz / 0x1000) {
         let off = page_index * 0x1000;
@@ -199,10 +241,23 @@ fn test_memory_read() {
     let mem_fd = unsafe { nc::open(qemu_memory_path, nc::O_RDONLY, 0) }.unwrap();
     let mut qemuMem = QemuMemoryView::new(mem_fd, &sorted_ranges).unwrap();
     assert_eq!(vec![1u8; 0x1000], qemuMem.read_block(0x0, 0x1000).unwrap());
-    assert_eq!(vec![2u8; 0x1000], qemuMem.read_block(0x1000, 0x1000).unwrap());
-    assert_eq!(vec![3u8; 0x1000], qemuMem.read_block(0x2000, 0x1000).unwrap());
+    assert_eq!(
+        vec![2u8; 0x1000],
+        qemuMem.read_block(0x1000, 0x1000).unwrap()
+    );
+    assert_eq!(
+        vec![3u8; 0x1000],
+        qemuMem.read_block(0x2000, 0x1000).unwrap()
+    );
     assert_eq!(true, qemuMem.read_block(0x3000, 0x1000).is_err());
-    let tmp = [vec![5u8; 0x1000], vec![6u8; 0x1000], vec![7u8; 0x1000], vec![8u8; 0x1000], vec![9u8; 0x1000]].concat();
+    let tmp = [
+        vec![5u8; 0x1000],
+        vec![6u8; 0x1000],
+        vec![7u8; 0x1000],
+        vec![8u8; 0x1000],
+        vec![9u8; 0x1000],
+    ]
+    .concat();
     assert_eq!(tmp, qemuMem.read_block(0x4000, 0x5000).unwrap());
     assert_eq!(tmp, qemuMem.read_block(0x4000, 0x5000).unwrap());
     assert_eq!(vec![0xb; 0x10], qemuMem.read_block(0xa000, 0x10).unwrap());
@@ -210,9 +265,12 @@ fn test_memory_read() {
     for range in sorted_ranges {
         for rel_offset in (0..range.gpa_extent).step_by(0x1000) {
             let offset = range.gpa_start + rel_offset;
-            tmp[offset..offset+0x1000].fill(((offset / 0x1000) + 1) as u8);
+            tmp[offset..offset + 0x1000].fill(((offset / 0x1000) + 1) as u8);
         }
     }
     assert_eq!(tmp, qemuMem.read_block(0, sz).unwrap());
-    assert_eq!([vec![0x8; 0x8], vec![0x9; 0x8]].concat(), qemuMem.read_block(0x7ff8, 0x10).unwrap());
+    assert_eq!(
+        [vec![0x8; 0x8], vec![0x9; 0x8]].concat(),
+        qemuMem.read_block(0x7ff8, 0x10).unwrap()
+    );
 }
